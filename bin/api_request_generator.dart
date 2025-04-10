3d462e18c0792f9397257c59bd1a583c76a5c132
abdo <a01226967080@gmail.com>
2025-04-10 16:42:07 +0200

    Add single action generation command and API configuration support

--- bin/api_request_generator.dart
+++ bin/api_request_generator.dart
@@ -1,11 +1,12 @@
 import 'package:api_request_generator/api_request_generator.dart';
 import 'package:api_request_generator/src/generate_action_command.dart';
+import 'package:api_request_generator/src/generate_single_action_command.dart';
 import 'package:args/command_runner.dart';
 
 void main(List<String> arguments) {
   final runner = CommandRunner('tool', 'A command line tool')
-
-    ..addCommand(GenerateActionsFromCollectionCommand());
+    ..addCommand(GenerateActionsFromCollectionCommand())
+    ..addCommand(GenerateSingleActionCommand());
 
   runner.run(arguments).catchError((error) {
     print('${ColorsText.red}Error: $error${ColorsText.reset}');
